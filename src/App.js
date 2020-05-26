1aaeaf56bf4ee53cf6aac62f8591d22dbd9b90fa
Ignacio Bejarano <ignbejarano@gmail.com>
2020-05-26 08:20:21 -0300

    changed app.js location

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import * as React from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
+
+import Header from "./components/header";
 
 const instructions = Platform.select({
   ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
   android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
 });
 
+const fetchFonts = () => {
+  return Font.loadAsync({
+    "pacifico-regular": require("../assets/fonts/Pacifico-Regular.ttf"),
+  });
+};
+
 export default function App() {
   return (
     <View style={styles.container}>
+      <Header />
       <Text style={styles.welcome}>Welcome to React Native!</Text>
       <Text style={styles.instructions}>To get started, edit App.js</Text>
       <Text style={styles.instructions}>{instructions}</Text>
@@ -19,18 +28,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
+    textAlign: "center",
     margin: 10,
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
+    textAlign: "center",
+    color: "#333333",
     marginBottom: 5,
   },
 });
